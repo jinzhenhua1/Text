20eb7ed877f109cdccbe62eb678bbcdca7bffdc1
jinzhenhua1 <374713003@qq.com>
2019-10-30 18:05:06 +0800

    MVVM模式简单应用

---
+++ app/src/main/java/com/example/text/DataBindActivity.java
@@ -0,0 +1,22 @@
+package com.example.text;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.text.bean.User;
+import com.example.text.databinding.ActivityDataBindBinding;
+
+public class DataBindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityDataBindBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_data_bind);
+        User user = new User();
+        binding.setUser(user);
+
+
+    }
+}
