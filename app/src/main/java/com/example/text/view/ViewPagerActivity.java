7eae233ecc412d154dfb21a7d6038ff01e08e129
jinzhenhua1 <374713003@qq.com>
2019-11-27 00:09:26 +0800

    viewpager

---
+++ app/src/main/java/com/example/text/view/ViewPagerActivity.java
@@ -0,0 +1,22 @@
+package com.example.text.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.text.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+    }
+
+
+}
