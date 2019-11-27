23f20d17ebb191353f03daa39af902ab50db6b42
jinzhenhua1 <374713003@qq.com>
2019-11-28 00:07:37 +0800

    懒加载

--- app/src/main/java/com/example/text/view/viewpager/BaseFragment.java
+++ app/src/main/java/com/example/text/view/viewpager/BaseFragment.java
@@ -19,8 +19,8 @@ import androidx.fragment.app.Fragment;
  */
 public abstract class BaseFragment extends Fragment {
     //双重判定，保证懒加载
-    protected boolean isVisible;//这个，标记，当前Fragment是否可见
-    private boolean isPrepared = false;//这个，标记当前Fragment是否已经执行了onCreateView
+    protected boolean isVisible;//这个，标记，当前Fragment是否可见，防止重复分发事件，如可见时还重复加载数据
+    private boolean isViewCreate = false;//view
     //只有两个标记同时满足，才进行数据加载
 
     protected View view;
@@ -29,31 +29,48 @@ public abstract class BaseFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(getLayoutId(),container,false);
+        view = inflater.inflate(getLayoutId(),null);
         initView(view);
-        isPrepared = true;
+        isViewCreate = true;
+        if(getUserVisibleHint()){
+            dispatchVisibleHint(true);//加载默认第一个页面时，setUserVisibleHint会在onCreateView前执行，所以会不执行页面的加载
+        }
         return view;
     }
 
+    /**
+     * 在viewPager中会调用该方法，来设置fragment是否可见
+     * @param isVisibleToUser
+     */
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        if (getUserVisibleHint()) {
-            isVisible = true;//这个方法和onCreateView存在先后顺序，如果这个方法先，那么isVisible就会先变成true，但是这个时候，isPrepared还不是true，所以，懒加载不会进行。而要等到onCreateView执行的时候。
-            onLazyLoad();
-        } else {
-            isVisible = false;
+
+        //当VIEW已经初始化，才能分发事件
+        if(isViewCreate){
+            if(isVisibleToUser && !isVisible){//本来不可见时，才会分发加载事件
+                dispatchVisibleHint(true);
+            }else if(!isVisibleToUser && isVisible){
+                dispatchVisibleHint(false);
+            }
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        if(!isVisible && getUserVisibleHint() && !isHidden()){
+            dispatchVisibleHint(true);
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
+
+        if(isVisible && getUserVisibleHint()){
+            dispatchVisibleHint(false);//跳转Activity时或者回到桌面时需要停止加载
+        }
     }
 
     @Override
@@ -61,22 +78,39 @@ public abstract class BaseFragment extends Fragment {
         super.onDestroyView();
     }
 
-    /**
-     * 懒加载
-     */
-    private void onLazyLoad() {
-        if (isPrepared && isVisible) {
-//                    processBar.setVisibility(View.GONE);
-            isPrepared = false;//懒加载，只加载一次,这句话要不要，就具体看需求
-            initData();
-        } else {
-            Log.d("onLazyLoadTag","拒绝执行initData，因为条件不满足");
-        }
-    }
-
     protected abstract void initView(View view);
 
     protected abstract int getLayoutId();
 
-    protected abstract void initData();
+
+    /**
+     * 事件分发
+     * @param visibleState
+     */
+    private void dispatchVisibleHint(boolean visibleState){
+        if(isVisible == visibleState){///在跨table显示时会分发多次事件
+            return;
+        }
+
+        isVisible = visibleState;
+        if(visibleState){
+            onFragmentLoad();
+        }else{
+            onFragmentLoadStop();
+        }
+    }
+
+    /**
+     * 加载数据
+     */
+    public void onFragmentLoad(){
+
+    }
+
+    /**
+     * 停止加载
+     */
+    public void onFragmentLoadStop(){
+
+    }
 }
